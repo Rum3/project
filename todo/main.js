1e489542f424456aaec1388bcdd840d2e740b640
Rum3 <105131287+Rum3@users.noreply.github.com>
2022-07-27 12:01:45 +0300

    Delete todo directory

--- todo/main.js
+++
@@ -1,9 +0,0 @@
-let addToDoButton = document.getElementById('addToDo');
-let toDoBox = document.getElementById('toDoBox');
-let inputField = document.getElementById('inputField');
-
-addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p')
-    paragraph.innerText = inputField.value;
-    toDoBox.appendChild(paragraph);
-})
